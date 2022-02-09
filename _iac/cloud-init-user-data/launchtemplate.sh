0e7a960190e3acb954b8701e4d79f3d4ddb8f259
Sanjay Mijar <sanjay@kontain.app>
2022-02-09 09:20:42 -0800

    add simple test and modified cloud-init

--- _iac/cloud-init-user-data/launchtemplate.sh
+++ _iac/cloud-init-user-data/launchtemplate.sh
@@ -1,10 +1,42 @@
 #!/bin/bash
+#set -e; [ "$TRACE" ] && set -x
+set -e
+set -x
+
+# figure out the latest release
+# This script must be run as root.
+[ `id -u` != "0" ] && echo "Must run as root" && exit 1
+
+source /etc/os-release
+[ "$ID" != "fedora" -a "$ID" != "ubuntu" -a "$ID" != "amzn" ] && echo "Unsupported linux distribution: $ID" && exit 1
+
+#TAG=${1:-$(curl -L -s https://raw.githubusercontent.com/kontainapp/km/current/km-releases/current_release.txt)}
+# forcing it to be latest
+TAG='latest'
+if [[ $TAG = latest ]] ; then
+   readonly URL_KONTAIN_TAR_GZ="https://github.com/kontainapp/km/releases/${TAG}/download/kontain.tar.gz"
+   readonly URL_KONTAIN_BIN="https://github.com/kontainapp/km/releases/${TAG}/download/kontain_bin.tar.gz"
+else
+   readonly URL_KONTAIN_TAR_GZ="https://github.com/kontainapp/km/releases/download/${TAG}/kontain.tar.gz"
+   readonly URL_KONTAIN_BIN="https://github.com/kontainapp/km/releases/download/${TAG}/kontain_bin.tar.gz"
+fi
+readonly PREFIX="/opt/kontain"
+
+
+# install yum-utils which contains needs-restarting to check if a reboot is necessary after update
+yum install -y yum-utils
 yum update -y
+
+# check if it needs rebooting while yum not running
+# ensure that cloud-init runs again after reboot by removing the instance record created
+(test ! -f /var/run/yum.pid && needs-restarting -r) || (rm -rf /var/lib/cloud/instances/;reboot)
+
 
 # install docker
 amazon-linux-extras install docker
 systemctl enable docker
-systemctl start --no-block docker
+# systemctl enable --now docker
+# start/restart at the end
 
 # and include ec2-user in docker group
 usermod -a -G docker ec2-user
@@ -17,17 +49,23 @@ chmod -v +x /usr/local/bin/docker-compose
 
 # install utils
 cd /tmp
-yum install -y jq
+yum install -y jq wget
 
-# download and install kontain
+# download and move files into place
 mkdir /tmp/kontain
 cd /tmp/kontain
-curl -o kkm.run -L  https://github.com/kontainapp/km/releases/download/v0.9.5/kkm.run
-chmod +x ./kkm.run && ./kkm.run
-mkdir -p /opt/kontain ; chown root /opt/kontain
-curl -s https://raw.githubusercontent.com/kontainapp/km/current/km-releases/kontain-install.sh | sudo bash
-
-# Now, install static krun
-curl -o kontain_bin.tar.gz -L  https://github.com/kontainapp/km/releases/download/v0.9.5/kontain_bin.tar.gz
+wget $URL_KONTAIN_BIN
 tar xvzf kontain_bin.tar.gz
-cp container-runtime/krun /opt/kontain/bin/
+
+# move files into /opt/kontain
+mkdir -p /opt/kontain/bin; chown root /opt/kontain
+mv container-runtime/krun /opt/kontain/bin/
+mv km/km /opt/kontain/bin/
+mv bin/docker_config.sh /opt/kontain/bin/
+
+# install KKM
+./kkm.run
+
+# configure and restart docker with Kontain as runtime
+# systemctl restart --no-block docker
+bash /opt/kontain/bin/docker_config.sh
